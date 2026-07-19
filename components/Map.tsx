629035a4b470fd655ca6b3c96702ca0c6d90b864
Wassim Hamwi <15844924+whamwi@users.noreply.github.com>
2026-07-19 22:06:32 +0400

    Switch to OpenStreetMap standard tiles

--- components/Map.tsx
+++ components/Map.tsx
@@ -49,9 +49,8 @@ export default function Map() {
         zoomControl: true,
       })
 
-      L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '© OpenStreetMap © CARTO',
-        subdomains: 'abcd',
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 19,
       }).addTo(map)
 
